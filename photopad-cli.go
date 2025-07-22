6b4dcffc472a762d0de8ea5fdf285cde56a1457e
riastrad <josherb4@gmail.com>
2025-07-22 21:57:38 +0530

    some plans

--- photopad-cli.go
+++ photopad-cli.go
@@ -5,12 +5,29 @@ import (
 	"os"
 )
 
+// TODO: all of it
+func readTxt(path string) {}
+
+func readPhoto(path string) {}
+
+func squishPixelsIntoPad() {}
+
+func shiftTxtWithPad() {}
+
+func unshiftTxtWithPad() {}
+
 func decode() 	{
 	fmt.Println("DEcode!")
+	// 1. func args should be the text loaded into memory & the pad
+	// 2. iterate through the txt & pad array, and for every latin character (shift the letter rightward)
+	// 3. save the encoded document next to the original
 }
 
 func encode() {
 	fmt.Println("ENcode!")
+	// 1. func args should be the text loaded into memory & the pad
+	// 2. iterate through the txt & pad array, and for every latin character (shift the letter rightward)
+	// 3. save the encoded document next to the original
 }
 
 func main() {
