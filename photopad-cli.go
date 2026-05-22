d90f9655897a21ce669e431a328e0dcd5e6df243
Josh Erb <josh@cyberb.space>
2026-05-22 14:55:19 +0530

    update cmd terminology, add help cmd, & setup go.mod file

--- photopad-cli.go
+++ photopad-cli.go
@@ -16,36 +16,56 @@ func shiftTxtWithPad() {}
 
 func unshiftTxtWithPad() {}
 
-func decode() 	{
-	fmt.Println("DEcode!")
+func decrypt() {
+	fmt.Println("🥚 message unscrambled!")
 	// 1. func args should be the text loaded into memory & the pad
 	// 2. iterate through the txt & pad array, and for every latin character (shift the letter rightward)
 	// 3. save the encoded document next to the original
 }
 
-func encode() {
-	fmt.Println("ENcode!")
+func encrypt() {
+	fmt.Println("🍳 message scrambled!")
 	// 1. func args should be the text loaded into memory & the pad
 	// 2. iterate through the txt & pad array, and for every latin character (shift the letter rightward)
 	// 3. save the encoded document next to the original
+}
+
+func printHelp() {
+	fmt.Print(`A picture's worth a thousand pads.™
+
+cmds:
+  help   : print this help message
+  encrypt: convert plaintext to ciphertext based on a provided .jpg file
+  decrypt: convert ciphertext to plaintext based on a provided .jpg file
+
+options:
+  --input, -i : path to a plaintext file
+  --output, -o: path to write the results of the operation [default=./{cmd}.output.txt]
+  --key, -k   : path to a valid .jpg file
+`)
+
 }
 
 func main() {
 	if len(os.Args) <= 1 {
-		fmt.Println("No arguments, hence nothing done.")
+		printHelp()
 		return
 	}
 
 	cmd := os.Args[1]
 
+	if cmd == "help" {
+		printHelp()
+		return
+	}
 
 	if cmd == "encode" {
-		encode()
+		encrypt()
 		return
 	}
 
 	if cmd == "decode" {
-		decode()
+		decrypt()
 		return
 	}
 }
